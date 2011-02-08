383f6958729b7d3867a12f689bce17dbc452d71d
Alexander Norström <alexander@norstrom.org>
2011-02-08 12:38:07 +0000

    labb2: Löste set-cdr! med dalenius hjälp.

--- labb2/labb2-4.rkt
+++ labb2/labb2-4.rkt
@@ -1,11 +1,17 @@
 (load "/home/TDDA69/Lab/r6rs/streams.ss")
-(require rnrs/mutable-pairs-6)
+
+; (require rnrs/mutable-pairs-6)
+
+(define-macro mcons-stream
+  (lambda (a b)
+    `(mcons ,a (delay ,b))))
+
 (define a
-  (cons-stream 1
-               (cons-stream 2
-                            (cons-stream 3 the-empty-stream))))
+  (mcons-stream 1
+               (mcons-stream 2
+                            (mcons-stream 3 the-empty-stream))))
 
 (define (my-cdr stream)
-  (set-cdr! stream
-            (cons (force (cdr stream))
-                  '(delay ,b))))
+  (set-mcdr! stream
+            (force (mcdr stream)))
+  (mcdr stream))
